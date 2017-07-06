82b6c937adb18c688d5d0a59ff22610e71d9adfb
abhishekarya286 <abhishekarya286@gmail.com>
2017-07-06 13:03:54 +0530

    cnn model for feature extraction

---
+++ cnn_model.lua
@@ -0,0 +1,190 @@
+require 'torch';
+require 'nn';
+require 'io';
+require 'lfs'
+require 'cunn'
+require 'cutorch'
+require 'nngraph'
+dofile 'opts.lua'
+require 'utilities.lua'
+require 'rnn'
+logger = require 'log'
+logger.outfile = opt.logFile
+print(opt)
+--define fillcolors for different layers
+COLOR_CONV = 'cyan';
+COLOR_MAXPOOL = 'grey';
+COLOR_RELU = 'lightblue';
+COLOR_SOFTMAX = 'green';
+COLOR_FC = 'orange';
+COLOR_AUGMENTS = 'brown';
+COLOR_LINEAR='pink'
+
+TEXTCOLOR = 'black';
+NODESTYLE = 'filled';
+lstm_hiddenstate=512
+lstm_inputsize  =256
+if opt.cnn_model=="alexnet" then
+	input_size=227
+else
+	input_size=224
+end		
+-- input dimensions:
+local nfeats = 3
+local width = input_size
+local height = input_size
+
+nnpackage = nn;
+CNN={}
+
+function CNN.cnn()
+
+	nngraph.setDebug(true)
+	print("creating new model " .. LOAD_MODEL_NAME .. '\n')
+	if opt.cnn_model=="alexnet" then
+		require 'loadcaffe'
+		pretrained_model=loadcaffe.load('deploy.prototxt','bvlc_alexnet.caffemodel','cudnn')
+    end	
+    cnn={}
+    table.insert(cnn,nn.Identity()())
+    print('creating cnn model')
+  
+	for k,v in pairs(pretrained_model) do
+		--print(v)
+		if(type(v)=='table') then
+			for i=1,table.getn(v) do
+				name=v[i].name:sub(1,4)
+				print(v[i].name)
+				if(name=="conv" and i==1) then
+					cnn['img_' .. v[i].name]=nnpackage.SpatialConvolution(v[i].nInputPlane, v[i].nOutputPlane, v[i].kW, v[i].kH, v[i].dW, v[i].dH, v[i].padW, v[i].padH)(cnn[1]):annotate{
+					name='image- convolution unit  ' .. v[i].name,
+					graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_CONV}
+				    }
+				elseif(name=="conv" and i>1) then
+					cnn['img_' .. v[i].name]=nnpackage.SpatialConvolution(v[i].nInputPlane, v[i].nOutputPlane, v[i].kW, v[i].kH, v[i].dW, v[i].dH, v[i].padW, v[i].padH)(cnn[prev]):annotate{
+					name='image- convolution unit  ' .. v[i].name,
+					graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_CONV}
+					}
+				elseif(name=="relu") then
+					cnn['img_' .. v[i].name]=nnpackage.ReLU()(cnn[prev]):annotate{
+					name='image- ReLU unit ' .. v[i].name,
+					graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_RELU}
+					}
+				elseif(name=="pool") then
+					cnn['img_' .. v[i].name]=nnpackage.SpatialMaxPooling(v[i].kW, v[i].kH, v[i].dW, v[i].dH, v[i].padW, v[i].padH)(cnn[prev]):annotate{
+					name='image- MAXPOOL unit ' .. v[i].name,
+					graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_MAXPOOL}
+					}	
+				elseif(name=="norm") then
+					cnn['img_' .. v[i].name]=nnpackage.SpatialCrossMapLRN(v[i].size,v[i].alpha,v[i].beta,v[i].k)(cnn[prev]):annotate{
+					name='image- localResponseNormalization unit ' .. v[i].name,
+					graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_MAXPOOL}
+					}
+				elseif(name=="torc") then	
+					break	
+				end
+				-- if(name=="conv" and num>1) then
+				-- 	print(subnet1['img1_' .. v[i].name])
+				-- 	subnet['img' ..string.format("%d",num) .. '_' .. v[i].name].data.module:share(subnet1['img1_' .. v[i].name].data.module,'weight', 'bias', 'gradWeight', 'gradBias')
+				-- end
+				prev='img_' .. v[i].name
+				print(prev)
+			end	
+		end	
+	end
+
+	---------------------------------------------------------------------------------------
+    --for alexnet the dimesnsion of the feature map is 256*6*6
+    cnn['img_avg_pool']=nnpackage.SpatialAveragePooling(6,6,6,6,0,0)(cnn['img_pool5']):annotate{
+	name='image AVGPOOL unit ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_MAXPOOL}
+	}	
+
+ 	cnn['img_view']=nnpackage.View(256)(cnn["img_avg_pool"]):annotate{
+	name='image view unit ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_MAXPOOL}
+	}
+
+	-- for initializing hidden states
+
+    cnn['img_mlp1_hid']=nnpackage.Linear(256,512)(cnn["img_view"]):annotate{
+	name='image Linear unit mlp1_hid ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_LINEAR}
+	}
+
+    cnn['img_mlp2_hid']=nnpackage.Linear(512,lstm_hiddenstate)(cnn['img_mlp1_hid']):annotate{
+	name='image Linear unit mlp12_hid ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_LINEAR}
+	}
+
+	-- for initializing cell states
+
+    cnn['img_mlp1_cell']=nnpackage.Linear(256,512)(cnn["img_view"]):annotate{
+	name='image Linear unit mlp1 ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_LINEAR}
+	}
+
+    cnn['img_mlp2_cell']=nnpackage.Linear(512,lstm_hiddenstate)(cnn['img_mlp1_cell']):annotate{
+	name='image Linear unit mlp12 ',graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_LINEAR}
+	}
+	outputs={}
+	table.insert(outputs,cnn['img_pool5'])
+	table.insert(outputs,cnn['img_mlp2_hid'])
+	table.insert(outputs,cnn['img_mlp2_cell'])
+	print(#outputs)
+	return{cnn,outputs}
+	--print(cnn,outputs)
+	--local model=nn.gModule(cnn,outputs)
+	--return model
+end	
+
+-- function create_model()
+-- 	nngraph.setDebug(true)
+-- 	print("creating new model " .. LOAD_MODEL_NAME .. '\n')
+-- 	if opt.cnn_model=="alexnet" then
+-- 		require 'loadcaffe'
+-- 		cnn=loadcaffe.load('deploy.prototxt','bvlc_alexnet.caffemodel','cudnn')
+--     end	
+--     --------------------------------------------------------------------------------------
+--     ---------------CNN for feature extraction
+
+-- 	---------------------------------------------------------------------------------------
+-- 	--Network for image 1 called as subnet-1
+-- 	---------------------------------------------------------------------------------------
+-- 	subnet1=cnn(opt.cnn_model,1)
+
+-- 	-------------------------------------------------------------------
+--     -- Network for the image-2 (called as subNetwork2
+--     --------------------------------------------------------------------------------
+--  --    subnet2=create_subnet_cnn(opt.cnn_model,2)
+
+-- 	-- -------------------------------------------------------------------
+--  --    -- Network for the image-3 (called as subNetwork3
+--  --    --------------------------------------------------------------------------------
+--  --    subnet3=create_subnet_cnn(opt.cnn_model,3)
+
+--  --    m3 = nn.JoinTable(1)({subnet1["img1_pool5"],subnet2["img2_pool5"],subnet3["img3_pool5"]}):annotate{
+-- 	-- 				      name='Joining unit',
+-- 	-- 				      graphAttributes = {color = TEXTCOLOR, style = NODESTYLE, fillcolor = COLOR_AUGMENTS}
+--  --        				 };
+--  --    ---------------------------------------------------------------------------------------
+--     -----------Attention Model
+    
+
+-- 	subnet1['img1_lstm']=nnpackage.LSTM()
+
+
+    
+
+--     model=localizeMemory(nn.gModule({subnet1["img1_conv1"]},{subnet1['img1_mlp2']}))
+--     --model = localizeMemory(nn.gModule({subnet1["img1_conv1"],subnet2["img2_conv1"],subnet3["img3_conv1"]},{m3}));
+-- 	model = localizeMemory(model);
+-- 	graph.dot(model.fg, 'model')  
+-- 	return model  
+-- end		
+
+--model=CNN.cnn()
+return CNN
+-- model = localizeMemory(model);
+-- graph.dot(model.fg, 'model','test') 
+
+-- output=model:forward(torch.randn(1,3,227,227))
+-- print(output[2])
+
+
+	
