443fb8ba0a64077b921bfdbc635dfeba4006ff98
abhishekarya286 <abhishekarya286@gmail.com>
2017-07-20 16:53:54 +0530

    added plottting

---
+++ plot.lua
@@ -0,0 +1,28 @@
+require 'gnuplot'
+
+function convert(csv)
+    local list = {}
+    for value in (csv .. ","):gmatch("(%S+)%W*,") do table.insert(list,tonumber(value))
+    print(value,"value") end
+    return unpack(list)
+end
+
+local file = io.open("logs/train1500535308.7603.log")
+i=0
+x=torch.Tensor()
+if file then
+	for line in file:lines() do
+		er1,er2=unpack(line:split(" "))
+		if(i>0) then
+			er1=convert(er1)
+			x=torch.cat(x,torch.Tensor({er1}));
+		end	
+		i=i+1
+	end
+end	
+
+gnuplot.pngfigure('logs/train.png')
+gnuplot.plot({'Train error',x})
+gnuplot.xlabel('iterations')
+gnuplot.ylabel('error')
+gnuplot.plotflush()
